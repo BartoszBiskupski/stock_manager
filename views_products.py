51d52d379281820589d8fd8a1a14da428fa5eab7
Bartosz Biskupski <bart.biskupski@gmail.com>
2018-04-01 21:50:58 +0200

    basic search working

--- views_products.py
+++ views_products.py
@@ -1,6 +1,6 @@
 from sqlite3 import IntegrityError
 
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from flask_login import login_user, login_required, logout_user, current_user
 from main import app, login_manager, db
 from forms import LoginForm, SignupForm
@@ -16,13 +16,22 @@ productsTemplate = 'products.html'
 def products():
     return render_template(productsTemplate)
 
-@app.route('/search', methods=['GET', 'POST'])
-def search():
+# @app.route('/search', methods=['GET', 'POST'])
+# def search():
+#
+#     return render_template(productsTemplate)
 
-    return render_template(productsTemplate)
-
-@app.route('/stock', methods=['GET'])
+@app.route('/stock', methods=['GET','POST'])
 def stock():
-    products = Product.query.order_by(Product.id)
-    return render_template(productsTemplate, products=products)
+    searched_products = []
+    query = request.args.get('query')
+    if query:
+        products = Product.query.order_by(Product.id)
+        for product in products:
+            if query in product.name:
+                searched_products.append(product)
+    else:
+        products = Product.query.order_by(Product.id)
+
+    return render_template(productsTemplate, products=products, searched_products=searched_products)
 
